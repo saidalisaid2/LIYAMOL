0e06768ad18e7ca81d9f6dbb2ed14b3802318265
saidali <86299543+saidalisaid2@users.noreply.github.com>
2021-09-18 10:22:40 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,12 +255,11 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                             whats = new Video(conn, msg);
                         } else {
                             whats = new Message(conn, msg);
-                        }
-/*
+                        
                         if (command.deleteCommand && msg.key.fromMe) {
                             await whats.delete(); 
                         }
-*/
+
                         try {
                             await command.function(whats, match);
                         } catch (error) {
